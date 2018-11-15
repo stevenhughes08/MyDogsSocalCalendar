33a55fb67da30c4ab5399e63639a21c277a616df
stevenhughes08 <stevenhughes08@gmail.com>
2018-11-14 23:02:58 -0500

    still not working api call

--- socialdog/src/features/user/PeopleDashboard/PeopleDashboard.jsx
+++ socialdog/src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
-//This is the PeopleDashboard page. This is a stateless functional component. 
+import React from 'react';
 
 
+class PeopleDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      people: []
+    };
+  }
 
-const PeopleDashboard = () => {
-  return (
-    <div>
-      <h1>People "Dog" Dashboard</h1>
-    </div>
-  )
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            people: result.people
+          });
+        },
+        //This is the error handling portion. 
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, people } = this.state;
+    if(error) {
+      return <div>Error: {error.message}</div>;
+    }else if (!isLoaded){
+      return <div>Loading...</div>;
+    }else{
+      return (
+        <ul>
+          {people.map(person => (
+          <li key={person.name}>
+          {person.name} {person.email} {person.address} 
+          </li>
+          ))}
+        </ul>
+      );
+    }
+  
+  }
 }
+
 
 export default PeopleDashboard

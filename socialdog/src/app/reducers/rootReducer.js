a5a819d5728d67f1a0bce3a04db030724d57df14
stevenhughes08 <stevenhughes08@gmail.com>
2018-11-20 06:23:07 -0500

    used redux forms, current version will not render

--- socialdog/src/app/reducers/rootReducer.js
+++ socialdog/src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux'
-import testReducer from './testReducer'
-import eventReducer from '../../features/event/eventReducer'
+import { combineReducers } from "redux";
+import { reducer as FormReducer } from "redux-form";
+import testReducer from "./testReducer";
+import eventReducer from "../../features/event/eventReducer";
 
 const rootReducer = combineReducers({
-    test: testReducer,
-    events: eventReducer
-})
+  form: FormReducer,
+  test: testReducer,
+  events: eventReducer
+});
 
 export default rootReducer;
